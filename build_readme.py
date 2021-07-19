c9e93ed036975dbf67f4a2c10255b75553916c9d
jsta <stachel2@msu.edu>
2021-07-19 09:02:08 -0600

    linting

--- build_readme.py
+++ build_readme.py
@@ -25,6 +25,7 @@ client = GraphqlClient(endpoint="https://api.github.com/graphql")
 # comment out below line for local builds
 TOKEN = os.environ.get("JSTA_TOKEN", "")
 
+
 def replace_chunk(content, marker, chunk, inline=False):
     r = re.compile(
         r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
@@ -97,9 +98,11 @@ def fetch_releases(oauth_token):
             if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
                 repos.append(repo)
                 repo_names.add(repo["name"])
-                
-                topics_raw = pd.io.json._normalize.nested_to_record(repo['repositoryTopics']['nodes'])
-                topics = []                
+
+                topics_raw = pd.io.json._normalize.nested_to_record(
+                    repo["repositoryTopics"]["nodes"]
+                )
+                topics = []
                 for topic in topics_raw:
                     topics.append(list(topic.values())[0])
                 topics = ", ".join(topics)
@@ -107,10 +110,10 @@ def fetch_releases(oauth_token):
                 releases.append(
                     {
                         "repo": repo["name"],
-                        "login": repo["owner"]["login"],                        
+                        "login": repo["owner"]["login"],
                         "repo_url": repo["url"],
                         "description": repo["description"],
-                        "keywords": topics,                        
+                        "keywords": topics,
                         "release": repo["releases"]["nodes"][0]["name"]
                         .replace(repo["name"], "")
                         .strip(),
@@ -126,14 +129,17 @@ def fetch_releases(oauth_token):
         after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
     return releases
 
+
 def fetch_blog_entries():
-    
+
     entries = feedparser.parse("https://jsta.rbind.io/blog/index.xml")["entries"]
     return [
         {
             "title": entry["title"],
             "url": entry["link"].split("#")[0],
-            "published": datetime.datetime.strptime(entry["published"], "%a, %d %b %Y %H:%M:%S %z").strftime("%Y-%m-%d")
+            "published": datetime.datetime.strptime(
+                entry["published"], "%a, %d %b %Y %H:%M:%S %z"
+            ).strftime("%Y-%m-%d"),
         }
         for entry in entries
     ]
@@ -143,17 +149,46 @@ if __name__ == "__main__":
     readme = root / "README.md"
     project_releases = root / "releases.md"
     releases = fetch_releases(TOKEN)
-    # remove bad releases    
-    releases = list(filter(lambda r: r["login"] not in ["ropenscilabs", "rbind"], releases))
-    releases = list(filter(lambda r: r["repo"] not in ["LAGOS_GIS_Toolbox", "LAGOSClimateSensitivity", "rgrass7sf", "tidybayes", "openbugs", "spnetwork", "LAGOS_NETS", "LivinOnTheEdge", "lagosus-reservoir"], releases))
-    
+    # remove bad releases
+    releases = list(
+        filter(lambda r: r["login"] not in ["ropenscilabs", "rbind"], releases)
+    )
+    releases = list(
+        filter(
+            lambda r: r["repo"]
+            not in [
+                "LAGOS_GIS_Toolbox",
+                "LAGOSClimateSensitivity",
+                "rgrass7sf",
+                "tidybayes",
+                "openbugs",
+                "spnetwork",
+                "LAGOS_NETS",
+                "LivinOnTheEdge",
+                "lagosus-reservoir",
+            ],
+            releases,
+        )
+    )
+
     # add missing releases
-    releases_missing = [{"repo": "smwrQW", "login": "USGS-R", "repo_url": "https://github.com/USGS-R/smwrQW", "description": "Water quality USGS water science R functions.", "keywords": "rstats, fortran", "release": "0.7.13", "published_at": "2017-07-24", "url": "https://github.com/USGS-R/smwrQW/releases/tag/v0.7.13"}]
-    releases.extend(releases_missing)    
- 
+    releases_missing = [
+        {
+            "repo": "smwrQW",
+            "login": "USGS-R",
+            "repo_url": "https://github.com/USGS-R/smwrQW",
+            "description": "Water quality USGS water science R functions.",
+            "keywords": "rstats, fortran",
+            "release": "0.7.13",
+            "published_at": "2017-07-24",
+            "url": "https://github.com/USGS-R/smwrQW/releases/tag/v0.7.13",
+        }
+    ]
+    releases.extend(releases_missing)
+
     releases.sort(key=lambda r: r["published_at"], reverse=True)
 
-    with open('releases.json', 'w') as outfile:
+    with open("releases.json", "w") as outfile:
         json.dump(releases, outfile)
 
     md = "\n".join(
