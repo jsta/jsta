d015417147b05c3282c18d2822b03d83c5930e2f
Jemma Stachelek <jsta@users.noreply.github.com>
2024-09-13 22:08:45 -0600

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -178,6 +178,7 @@ if __name__ == "__main__":
                 "tebaldi-etal_2021_natclimchange",
                 "tropicalcyclone_MLP",
                 "icom-mesh-data",
+                "CRB-human-impacts"
             ],
             releases,
         )
