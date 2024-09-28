8047b8fda1ed88fa287963628ae0c0b2c801edec
Jemma Stachelek <jsta@users.noreply.github.com>
2024-09-27 21:04:05 -0600

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -250,6 +250,6 @@ if __name__ == "__main__":
     # entries_md = "\n".join(
     #     ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
     # )
-    rewritten = replace_chunk(rewritten, "blog", "![https://ed-hawkins.github.io/climate-visuals/PALEO-STRIPES/PAGES2k-BARS-1-2023-black.png](https://ed-hawkins.github.io/climate-visuals/PALEO-STRIPES/PAGES2k-BARS-1-2023-black.png)")
+    rewritten = replace_chunk(rewritten, "blog", "<a href='https://ed-hawkins.github.io/climate-visuals/PALEO-STRIPES/PAGES2k-BARS-1-2023-black.png" width=300/></a>")
 
     readme.open("w").write(rewritten)
